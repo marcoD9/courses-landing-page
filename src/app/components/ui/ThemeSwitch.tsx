1c13b53d643a424b39e7609d28402f6a1e030b3c
Marco <marco.dmail96@gmail.com>
2025-06-01 16:30:12 +0200

    Update comments

--- src/app/components/ui/ThemeSwitch.tsx
+++ src/app/components/ui/ThemeSwitch.tsx
@@ -6,22 +6,24 @@ import styles from "./ThemeSwitch.module.css";
 export default function ThemeSwitch() {
   const [isOn, setIsOn] = useState(false);
 
+  // useEffect to switch theme
   useEffect(() => {
     const htmlElement = document.documentElement;
     if (isOn) {
       htmlElement.classList.add("dark");
-      htmlElement.classList.remove("light"); // Remove "light" if you don't use it as base class
+      htmlElement.classList.remove("light"); // Remove "light" if you don't use it as default class
     } else {
       htmlElement.classList.remove("dark");
-      htmlElement.classList.add("light"); // Add 'light' if you use it as base class
+      htmlElement.classList.add("light"); // Add 'light' if you use it as default class
     }
     localStorage.setItem("theme-preference", isOn ? "dark" : "light");
-  }, [isOn]);
+  }, [isOn]); // Update every time the switch is toggled
 
+  // useEffect to check the default theme to apply
   useEffect(() => {
     const savedTheme = localStorage.getItem("theme-preference");
     const htmlElement = document.documentElement;
-
+    // Set the theme depending on browser setting
     if (savedTheme) {
       setIsOn(savedTheme === "dark");
       if (savedTheme === "dark") {
