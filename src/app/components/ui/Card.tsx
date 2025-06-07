2644b74d1770fff0a3e1ae5aec9000eefadf0c79
Marco <marco.dmail96@gmail.com>
2025-06-07 11:57:46 +0200

    Update Card to accept an image as prop for the background

--- src/app/components/ui/Card.tsx
+++ src/app/components/ui/Card.tsx
@@ -2,9 +2,11 @@ import * as motion from "motion/react-client";
 
 interface CardProps {
   content: string;
+  image: string;
 }
 
-export default function Card({ content }: CardProps) {
+export default function Card({ content, image }: CardProps) {
+  const imageUrl = `/${image}`;
   return (
     <motion.div
       whileHover={{
@@ -16,8 +18,9 @@ export default function Card({ content }: CardProps) {
         },
       }}
       whileTap={{ scale: 0.8 }}
-      className=" bg-[var(--card)] text-[var(--text-secondary] h-full rounded-4xl content-center justify-items-center font-bold hover:bg-[var(--card-hover-color)]"
+      className=" bg-[var(--card)] text-[var(--text-secondary] h-full rounded-4xl content-center justify-items-center font-bold  overflow-hidden    bg-cover bg-center bg-no-repeat  hover:bg-[var(--card-hover-color)]"
       key={content}
+      style={{ backgroundImage: `url('${imageUrl}` }}
     >
       <p>{content}</p>
     </motion.div>
