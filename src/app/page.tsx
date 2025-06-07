e373951c8d0647ac6d17613aa21f592c4c644a92
Marco <marco.dmail96@gmail.com>
2025-06-07 17:37:53 +0200

    Update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Section from "./components/Section";
 import TitleBar from "./components/TitleBar";
+import SectionTitle from "./components/SectionTitle";
 
 export default function Home() {
   return (
     <div className="overflow-hidden">
       <TitleBar />
+      <SectionTitle />
       <Section />
     </div>
   );
