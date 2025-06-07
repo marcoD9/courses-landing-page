1a4c0942a9fa0bcbdb524b378a54d8f4a1023273
Marco <marco.dmail96@gmail.com>
2025-06-07 11:44:22 +0200

    Add padding

--- src/app/components/Section.tsx
+++ src/app/components/Section.tsx
@@ -2,7 +2,7 @@ import Card from "./ui/Card";
 
 export default function Section() {
   return (
-    <div className="bg-[var(--background)] grid h-[60vh] border-6 p-8 mt-4 border-[var(--border)] rounded-2xl gap-8 grid-cols-3 grid-rows-3">
+    <div className="bg-[var(--background)] grid h-[60vh] border-6 p-2 mt-4 mx-2 border-[var(--border)] rounded-2xl gap-2 grid-cols-3 grid-rows-3">
       <div className="col-span-1 row-span-3">
         <Card content="1" />
       </div>
