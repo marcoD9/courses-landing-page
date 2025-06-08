19a0c65e1ef39446a194b9e1a5431067d4c77d83
Marco <marco.dmail96@gmail.com>
2025-06-08 12:12:15 +0200

    Update section title

--- src/app/components/SectionTitle.tsx
+++ src/app/components/SectionTitle.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function SectionTitle() {
   return (
-    <div className="flex flex-row  justify-center">
+    <div className=" title-secondary flex flex-row  justify-center">
       <h2>Courses</h2>
     </div>
   );
